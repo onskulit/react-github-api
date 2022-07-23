7c92a5475391499378f0798333911bde5a776e0e
onskulit <dislaty@mail.com>
2022-07-23 13:29:19 +0300

    feat: add favorites functionality

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useActions } from "../hooks/actions";
+import { useAppSelector } from "../hooks/redux";
 import { IRepo } from "../models/models";
 
 export function RepoCard({ repo }: { repo: IRepo }) {
+  const { addFavorite, removeFavorite } = useActions();
+  const { favorites } = useAppSelector(state => state.github);
+  const [isFav, setIsFav] = useState(favorites.includes(repo.html_url));
+
+  const addToFavorite = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    addFavorite(repo.html_url);
+    setIsFav(true);
+  }
+
+  const removeFromFavorite = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    removeFavorite(repo.html_url);
+    setIsFav(false);
+  }
+
   return (
     <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
       <a href={ repo.html_url } target="_blank">
@@ -13,6 +31,12 @@ export function RepoCard({ repo }: { repo: IRepo }) {
           Watchers: <span className="font-bold">{repo.watchers}</span>
         </p>
         <p className="text-sm font-thin">{ repo?.description }</p>
+
+        { !isFav && <button className="py-2 px-4 bg-yellow-400 mr-2 rounded hover:shadow-md transition-all"
+        onClick={addToFavorite}>Add</button> }
+
+        { isFav && <button className="py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all"
+        onClick={removeFromFavorite}>Remove</button>}
       </a>
     </div>
   )
--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useAppSelector } from "../hooks/redux";
 
 export function FavoritesPage () {
+  const { favorites } = useAppSelector(state => state.github);
+
+  if (favorites.length === 0) return <p className="text-center">No items.</p>
+
   return (
-    <div>Favorites</div>
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      <ul className="list-none">
+        { favorites.map(fav => (
+          <li key={fav}>
+            <a href={fav} target="_blank">{ fav }</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+    
   )
 }
